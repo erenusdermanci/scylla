a6cebf31a04a76f407becc618c89a729a62f0c1c
Peter Goron <peter.goron@gmail.com>
2023-08-14 09:14:41 +0200

    auth: add support for authc/authz via external api (#10)
    backport of 801fc8e5a7a295710968384af5a0daf7606ed932 from
    v5.2.17-criteo branch.
    
    Signed-off-by: Erenus Dermanci <erenus@mail.com>

--- configure.py
+++ configure.py
@@ -8,6 +8,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
 
+#
+# Modified by Criteo: June 2021
+#
+
 import argparse
 import os
 import platform
@@ -571,6 +575,7 @@ scylla_tests = set([
     'test/boost/reusable_buffer_test',
     'test/boost/restrictions_test',
     'test/boost/repair_test',
+    'test/boost/rest_authenticator_test',
     'test/boost/role_manager_test',
     'test/boost/row_cache_test',
     'test/boost/rust_test',
@@ -1139,6 +1144,8 @@ scylla_core = (['message/messaging_service.cc',
                 'auth/roles-metadata.cc',
                 'auth/passwords.cc',
                 'auth/password_authenticator.cc',
+                'auth/rest_authenticator.cc',
+                'auth/rest_role_manager.cc',
                 'auth/permission.cc',
                 'auth/permissions_cache.cc',
                 'auth/service.cc',
