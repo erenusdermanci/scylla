e16d35663a184063e63d0075bafb720afbfa8212
Peter Goron <p.goron@criteo.com>
2024-04-12 18:11:30 +0200

    [criteo] mv: sleep a bit before view-update-generator restart
    The "view update generator" is responsible for generating view updates
    for staging sstables (such as coming from repair). If the processing
    fails, the code retries - immediately. If there is some persistent bug,
    such as issue scylladb#16598, we will have a tight loop of error messages,
    potentially a gigabyte of identical messages every second.
    
    In this patch we simply add a sleep of one second after view update
    generation fails before retrying. We can still get many identical
    error messages if there is some bug, but not more than one per second.
    
    Refs scylladb#16598.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    
    Backported on v5.2.17-criteo (see https://github.com/scylladb/scylladb/issues/18222)

--- db/view/view_update_generator.cc
+++ db/view/view_update_generator.cc
@@ -179,6 +179,9 @@ future<> view_update_generator::start() {
                     // Need to add sstables back to the set so we can retry later. By now it may
                     // have had other updates.
                     std::move(sstables.begin(), sstables.end(), std::back_inserter(_sstables_with_tables[t]));
+                    // See https://github.com/scylladb/scylladb/issues/18222
+                    // Sleep a bit, to avoid a tight loop repeatedly spamming the log with the same message.
+                    seastar::sleep(std::chrono::seconds(1)).get();
                     break;
                 }
                 try {
